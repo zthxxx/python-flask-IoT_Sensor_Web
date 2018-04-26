a4fd36df0ede04270ec366c626dadf8bf947d4de
zthxxx <zthxxx.me@gmail.com>
2018-04-27 00:24:52 +0800

    feat: add mongo mirror

--- IoT_web_docker_init.sh
+++ IoT_web_docker_init.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-
-COMMENTS='
-#Docker image from ubuntu version >= 14.04
-#Docker image run command example:
-#:~$ wget https://raw.githubusercontent.com/zthxxx/python-flask-IoT_Sensor_Web/feature/IoT_web_docker_init.sh
-#:~$ docker run -dit -v /var/database/mongodb:/var/database/mongodb -p 80:80 -p 31511:31511 -p 3000:3000 ubuntu /bin/bash
-#:~$ cat IoT_web_docker_init.sh | docker exec -i $(docker ps -q) tee /root/IoT_web_docker_init.sh
-#:~$ docker exec $(docker ps -q) chmod +x /root/IoT_web_docker_init.sh
-#:~$ docker exec $(docker ps -q) /root/IoT_web_docker_init.sh | docker exec -i $(docker ps -q) tee -a /root/docker_init.log
-'
+# plz see usage in README
 
 
-apt-get update
-yes | apt-get install wget
-yes | apt-get install vim
-echo '
-set number
-set relativenumber
-filetype on
-set history=1000
-set background=dark
-syntax on
-set autoindent
-set smartindent
-set tabstop=4
-set shiftwidth=4
-set showmatch
-set ruler
-set nowrap
-set textwidth=0
-set hlsearch
-' | tee ~/.vimrc
+apt update
+apt install -y wget vim
+wget -q git.io/tvimrc -O ~/.vimrc
 
 ########## python3.5 setup
-yes | apt-get install python3.5
-yes | apt-get install python3.5-dev
+apt install -y python3.5 python3.5-dev
 cd ~
 wget https://bootstrap.pypa.io/ez_setup.py
 python3.5 ez_setup.py
@@ -43,7 +16,7 @@ easy_install-3.5 pip
 rm -rf ez_setup.py setuptools-*.zip
 
 ########## git setup
-yes | apt-get install git
+apt-get install git
 git config --global user.name "tian_ubuntu_docker"
 git config --global user.email "zth_9451@qq.com"
 rm -rf ~/.ssh/
@@ -52,10 +25,12 @@ eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 
 ########## mongodb setup
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+# mirror ref: https://mirror.tuna.tsinghua.edu.cn/help/mongodb/
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/mongodb/apt/ubuntu trusty/mongodb-org/stable multiverse" | tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
 apt-get install -y mongodb-org
+
 MONGO_PATH="/var/database/mongodb"
 mkdir -p $MONGO_PATH/db
 mkdir -p $MONGO_PATH/log
@@ -79,8 +54,7 @@ echo 'auth = true' >> /etc/mongodb.conf
 mongod --config /etc/mongodb.conf &
 
 ######### nodejs & npm
-yes | apt-get install nodejs
-yes | apt-get install npm
+apt-get install nodejs npm
 
 ######### flask web pull
 PROJECT_NAME="Project/Python/FlaskProject"
@@ -88,7 +62,7 @@ cd ~
 mkdir -p $PROJECT_NAME
 cd $PROJECT_NAME
 wget https://github.com/zthxxx/python-flask-IoT_Sensor_Web/archive/feature.zip -O python-flask-IoT_Sensor_Web.zip
-yes | apt-get install unzip
+apt-get install unzip
 unzip python-flask-IoT_Sensor_Web.zip
 rm -f python-flask-IoT_Sensor_Web.zip
 mv python-flask-IoT_Sensor_Web-feature python-flask-IoT_Sensor_Web
